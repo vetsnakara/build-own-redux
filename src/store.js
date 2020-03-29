71762794dfbcc11f85bda18640506467bbd7f1d7
Konstantin Arakantsev <kostya3312@gmail.com>
2020-03-30 00:20:39 +0300

    feat: listening to changes

--- src/store.js
+++ src/store.js
@@ -1,10 +1,20 @@
 const createStore = () => {
   let state
+  let listeners = []
 
   const getState = () => state
 
+  const subscribe = listener => {
+    listeners.push(listener)
+
+    return () => {
+      listeners = listeners.filter(l => l !== listener)
+    }
+  }
+
   return {
-    getState
+    getState,
+    subscribe
   }
 }
 
