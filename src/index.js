e74a498b63736f5380b42a64b8619a34048c0d56
Konstantin Arakantsev <kostya3312@gmail.com>
2020-03-30 15:18:01 +0300

    feat: add constants for action types

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import { createStore } from './store'
 
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
 const todos = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_TODO':
+    case ADD_TODO:
       return state.concat(action.todo)
-    case 'REMOVE_TODO':
+    case REMOVE_TODO:
       return state.filter(todo => todo.id !== action.id)
-    case 'TOGGLE_TODO':
+    case TOGGLE_TODO:
       return state.map(todo => (
         todo.id === action.id
           ? { ...todo, complete: !todo.complete }
@@ -19,9 +26,9 @@ const todos = (state = [], action) => {
 
 const goals = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_GOAL':
+    case ADD_GOAL:
       return [...state, action.goal]
-    case 'REMOVE_GOAL':
+    case REMOVE_GOAL:
       return state.filter(goal => goal.id !== action.id)
     default:
       return state
@@ -38,7 +45,7 @@ const store = createStore(rootReducer)
 store.subscribe(() => console.log('The new state:', store.getState()))
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 0,
     name: 'Learn Redux',
@@ -47,7 +54,7 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 1,
     name: 'Learn Pure functions',
@@ -56,17 +63,17 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'REMOVE_TODO',
+  type: REMOVE_TODO,
   id: 0
 })
 
 store.dispatch({
-  type: 'TOGGLE_TODO',
+  type: TOGGLE_TODO,
   id: 1
 })
 
 store.dispatch({
-  type: 'ADD_GOAL',
+  type: ADD_GOAL,
   goal: {
     id: 0,
     name: 'Lose 10 kg'
@@ -74,7 +81,7 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'ADD_GOAL',
+  type: ADD_GOAL,
   goal: {
     id: 1,
     name: 'Learn to fly'
@@ -82,6 +89,6 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'REMOVE_GOAL',
+  type: REMOVE_GOAL,
   id: 1
 })
